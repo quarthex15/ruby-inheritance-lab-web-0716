912163d1a81b843008cdd582db150cb2588cf2c7
Andrew Klein <andrew.klein.j@gmail.com>
2016-08-01 09:28:52 -0400

    complete assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(new_info)
+    @knowledge << new_info
+  end
+
 
 end
